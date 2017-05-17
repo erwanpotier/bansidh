38f20aeb35888620cd8c84c98d192ee0faf49aed
erwanpotier <erwan-potier@hotmail.fr>
2017-05-17 19:06:49 +0200

    update javascript theme

--- themes/bansidh/js/array.select.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function() {
-
-	//jQuery("#edit-categories").megaselectlist({ animate: true, animateevent: "click" });
-
-
-
-	//jQuery("#edit-field-categorie").megaselectlist({ animate: true, animateevent: "click" });
-
-});
